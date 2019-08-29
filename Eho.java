485da56aa30e66b5d3b351b51ecc1cd674cb7cd5
Sanjay kr <43911014+sk720@users.noreply.github.com>
2019-08-29 23:39:41 +0530

    Add files via upload

---
+++ Eho.java
@@ -0,0 +1,10 @@
+public class Eho
+{
+  public static void main(String []args)
+  {
+   int s=0;
+  for(int i =0; i<args.length; i++)
+ s=s+Integer.parseInt(args[i]);
+  System.out.println("sum is "+s);
+  }
+}
---
+++ class.java
@@ -0,0 +1,14 @@
+public class Example //outer class
+
+{
+
+ int x;
+void fun(){
+}
+  class Dummy//inner class
+  
+{
+  
+  }
+
+}
